c905c2f10493723dc84ec7ee6bcae1910974dd9c
y80218md <mehmet.demir@student.manchester.ac.uk>
2021-02-18 06:24:19 +0000

    Input Validation

--- chess/CheckInput.java
+++ chess/CheckInput.java
@@ -1,9 +1,17 @@
 package chess;
 
 public class CheckInput {
-	
+
 	//This method requires your input
 	public boolean checkCoordinateValidity(String input){
+		if (input.length() != 2) return false;
+		else {
+			char row = input.charAt(0);
+			int row_ascii = (int) row;
+			char col = input.charAt(1);
+			int col_ascii = (int) col;
+			if (row_ascii <= 56 && row_ascii >= 49 && col_ascii <= 104 && col_ascii >= 97) return true;
+		}
 		return false;
 	}
 }
--- chess/Game.java
+++ chess/Game.java
@@ -1,4 +1,5 @@
 package chess;
+import java.util.Scanner;  // Import the Scanner class
 
 public class Game {
 	private static boolean gameEnd=false;
@@ -6,9 +7,9 @@ public class Game {
 	//This method requires your input
 	public static void play(){
 		while (!gameEnd){
-		}		
+		}
 	}
-	
+
 	//This method should not be edited
 	public static void main (String args[]){
 		Board.initialiseBoard();
